b31995c8a8f3f8edd847168a869d1a42e1dba4cc
Vaibhav-Sonkusare <vaibhavsonkusare2@gmail.com>
2025-09-17 08:50:33 +0530

    feat: display the longest strictly increasing subsequence
    - Added tracking of predecessors using the `prev` array
    - Reconstructed the sequence using backtracking from the dp array
    - Printed the subsequence along with its length

--- DSA/Lab_4_10Sep2025/longest increasing substring.c
+++ DSA/Lab_4_10Sep2025/longest increasing substring.c
@@ -9,26 +9,46 @@ int max(int, int);
 int main() {
     int nums[] = {8, 2, 5, 1, 6, 3, 4, 7, 6};
     int size = sizeof(nums) / sizeof(int);
-    int max_len = 0, end;
+    int max_len = 1, end = 0;
 
     int dp[size];
+    int prev[size];
 
     for (int i=0; i< size; i++) {
         dp[i] = 1;
+        prev[i] = -1;
+    }
+
+    for (int i=0; i< size; i++) {
         for (int j = i - 1; j >= 0; j--) {
-            if (nums[i] > nums[j]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-                if (max_len < dp[i]) {
-                    max_len = dp[i];
-                    end = i;
-                }
+            if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+                prev[i] = j;
             }
             // printf("dp[%d] = %d\n", i, dp[i]);
+        }
+        if (dp[i] > max_len) {
+            max_len = dp[i];
+            end = i;
         }
     }
 
     printf("Max Length of Substring can be %d.\n", max_len);
 
+    // Print the longest strictly increasing subsequence
+    int seq[max_len];
+    int i = max_len - 1;
+    int curr = end;
+    while (curr != -1) {
+        seq[i] = nums[curr];
+        i--;
+        curr = prev[curr];
+    }
+    printf("Longest Increasing Subsequence: ");
+    for (int i=0; i< max_len; i++) {
+        printf("%d ", seq[i]);
+    }
+    printf("\n");
 
     return 0;
 }
