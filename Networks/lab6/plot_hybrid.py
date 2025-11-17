86865da544151a9e0fe7ce4bb1ab31e666b168ef
NITT/CSE/MTECH/CF:OH-35/2024-25/19 <vaibhavsonkusare2@gmail.com>
2025-11-17 16:51:45 +0530

    feat: Networks/lab6

---
+++ Networks/lab6/plot_hybrid.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Ns = [5, 10, 15, 20, 25]
+throughput = []
+packet_loss = []
+
+for N in Ns:
+    df = pd.read_csv(f"/home/mtech/ns-allinone-3.38/ns-3.38/results_hybrid_{N}.csv")
+    throughput.append(df['Throughput_Mbps'][0])
+    packet_loss.append(df['PacketLoss'][0])
+
+# Graph 1: Throughput vs Number of Nodes
+plt.figure()
+plt.plot(Ns, throughput, marker='o')
+plt.xlabel("Number of Nodes")
+plt.ylabel("Throughput (Mbps)")
+plt.title("Throughput vs Number of Nodes (Hybrid Topology)")
+plt.grid(True)
+plt.show()
+plt.savefig("hybrid_Throughput_vs_Number_of_Nodes.png")
+
+# Graph 2: Packet Loss vs Number of Nodes
+plt.figure()
+plt.plot(Ns, packet_loss, marker='o')
+plt.xlabel("Number of Nodes")
+plt.ylabel("Packet Loss")
+plt.title("Packet Loss vs Number of Nodes (Hybrid Topology)")
+plt.grid(True)
+plt.show()
+plt.savefig("hybrid_Packet_Loss_vs_Number_of_Nodes.png")
